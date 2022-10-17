7dc5f2cf3f11bcd92c92879a34c49ec920877dd5
Carlos Frinka Neto <c.frinkaneto@gmail.com>
2022-10-17 10:31:09 -0300

    uiplodas

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,5 @@
 class MainController < ApplicationController
     def index    
-        flash[:notice] = "Logged in sucessfully"
-        flash[:alert] = "Invalid e-mail or password"
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Rails.application.routes.draw do
 
   root to: "main#index"
 
-  get "/about-us", to: "about#index", as: :about
+  get "/about", to: "about#index", as: :about
+
+  get '/password', to: 'passwords#edit', as: :edit_password
+  patch '/password', to: 'passwords#update'
+
+  get '/sign_up', to: 'registration#new'
+  post '/sign_up', to: 'registration#create'
+
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+
+
+  delete '/logout', to: 'sessions#destroy'
 
 end
